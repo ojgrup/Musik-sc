398d4fb26b98bcd2b7dd027f8f1deefa57675bd3
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-30 15:46:12 +0800

    Create karaoke_songs.dart

---
+++ lib/data/karaoke_songs.dart
@@ -0,0 +1,6 @@
+// lib/data/karaoke_songs.dart
+import '../models.dart';
+
+final List<SongModel> karaokeSongs = const [
+  SongModel(title: "Lagu K (Karaoke)", artist: "Instrumental", assetFileName: "Lagu_K_Karaoke.mp3", duration: Duration(minutes: 4, seconds: 05)),
+];
