a4866aa1ae5698904958413200c734a5bd18f302
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-30 15:43:35 +0800

    Create original_songs.dart

---
+++ lib/data/original_songs.dart
@@ -0,0 +1,7 @@
+// lib/data/original_songs.dart
+import '../models.dart';
+
+final List<SongModel> originalSongs = const [
+  SongModel(title: "Lagu A (Original)", artist: "Artis X", assetFileName: "Lagu_A_Original.mp3", duration: Duration(minutes: 4, seconds: 15)),
+  SongModel(title: "Lagu B (Original)", artist: "Artis Y", assetFileName: "Lagu_B_Original.mp3", duration: Duration(minutes: 3, seconds: 30)),
+];
