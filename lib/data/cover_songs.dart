e78949d5bc64d3b73822d85b31b99216c42e566e
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-30 15:44:25 +0800

    Create cover_songs.dart

---
+++ lib/data/cover_songs.dart
@@ -0,0 +1,7 @@
+// lib/data/cover_songs.dart
+import '../models.dart';
+
+final List<SongModel> coverSongs = const [
+  SongModel(title: "Lagu A (Cover)", artist: "Cover Artis 1", assetFileName: "Lagu_A_Cover.mp3", duration: Duration(minutes: 4, seconds: 10)),
+  SongModel(title: "Lagu C (Cover)", artist: "Cover Artis 2", assetFileName: "Lagu_C_Cover.mp3", duration: Duration(minutes: 3, seconds: 50)),
+];
