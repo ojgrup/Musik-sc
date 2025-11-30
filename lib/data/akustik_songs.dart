96b480f9ee38012467d853bcfd2c8bea2767439d
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-30 15:45:22 +0800

    Create akustik_songs.dart

---
+++ lib/data/akustik_songs.dart
@@ -0,0 +1,17 @@
+// lib/data/akustik_songs.dart
+import '../models.dart';
+
+final List<SongModel> akustikSongs = const [
+  SongModel(
+    title: "Peterpan - Walau Habis Terang", 
+    artist: "Peterpan", 
+    assetFileName: "Peterpan_Walau_H...mp3", 
+    duration: Duration(minutes: 4, seconds: 40)
+  ),
+  SongModel(
+    title: "Lagu Rahasia Akustik", 
+    artist: "Unknown Artist", 
+    assetFileName: "Lagu.mp3", 
+    duration: Duration(minutes: 3, seconds: 10)
+  ),
+];
